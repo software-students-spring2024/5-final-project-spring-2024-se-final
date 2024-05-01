536b09d56645092855c9b7946598eb71407dcfa6
ZhongqianChen <zc1545@nyu.edu>
2024-05-01 01:02:28 -0400

    Port Update

--- web-app/app.py
+++ web-app/app.py
@@ -158,4 +158,4 @@ def delete_location(project_id, location_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
